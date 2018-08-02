934a067c343908da34c0527e15c757700011d1e5
Briana Lane Indahl <blindahl@astro.as.utexas.edu>
2018-08-02 16:37:58 -0500

    build emcee functions into class

--- emission_line_emcee_functions.py
+++ emission_line_emcee_functions.py
@@ -4,50 +4,49 @@ import math
 import os
 import model_line_functions as mlf
 
-# class MCMC_functions(model, model_bounds):
-# 	def __init__(self):
-# 		self.model = model
-# 		self.model_args = model_args
-# 		self.model_bounds = model_bounds
+class MCMC_functions():
+	def __init__(self, model, model_bounds):
+		self.model = model
+		self.model_bounds = model_bounds
 
-#define the log likelihood function
-def lnlike(theta, x, y, yerr):
-	mod = mlf.OIII_doub_gaussian(x, theta)
-	return -0.5*sum(((y - mod)**2)/(yerr**2))
+	#define the log likelihood function
+	def lnlike(self, theta, x, y, yerr):
+		mod = self.model(x, theta)
+		return -0.5*sum(((y - mod)**2)/(yerr**2))
 
-#define the log prior function
-def lnprior(theta):
-	# if len(theta) > 3:
-	# 	if ( 0.0005 <= theta[0] <= 0.002) and (3.5 <= theta[1] <= 4.5) and (0.0 <= theta[2] <= 100) and (0.0 <= theta[3] <= 100):
-	# 		return 0.0
-	# 	return -np.inf
-	# else:
-	if ( 0.0005 <= theta[0] <= 0.002) and (3.5 <= theta[1] <= 4.5) and (0.0 <= theta[2] <= 100):
-		return 0.0
-	return -np.inf
-
-#define log postierior to sovle with emcee
-def lnprob(theta, x, y, yerr):
-	lp = lnprior(theta)
-	if not np.isfinite(lp):
+	#define the log prior function
+	def lnprior(self, theta):
+		# if len(theta) > 3:
+		# 	if ( 0.0005 <= theta[0] <= 0.002) and (3.5 <= theta[1] <= 4.5) and (0.0 <= theta[2] <= 100) and (0.0 <= theta[3] <= 100):
+		# 		return 0.0
+		# 	return -np.inf
+		# else:
+		if ( 0.0005 <= theta[0] <= 0.002) and (3.5 <= theta[1] <= 4.5) and (0.0 <= theta[2] <= 20):
+			return 0.0
 		return -np.inf
-	return lp + lnlike(theta, x, y, yerr)
 
-def run_emcee(lnprob, ndim, nwalkers, nchains, thetaGuess, args):
+	#define log postierior to sovle with emcee
+	def lnprob(self, theta, x, y, yerr):
+		lp = self.lnprior(theta)
+		if not np.isfinite(lp):
+			return -np.inf
+		return lp + self.lnlike(theta, x, y, yerr)
 
-	pos = [thetaGuess + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
-	sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=args)
+	def run_emcee(self, lnprob, ndim, nwalkers, nchains, thetaGuess, args):
 
-	print "Burning in ..."
-	pos, prob, state = sampler.run_mcmc(pos, 200)
+		pos = [thetaGuess + 1e-4*np.random.randn(ndim) for i in range(nwalkers)]
+		sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args=args)
 
-	sampler.reset()
+		print "Burning in ..."
+		pos, prob, state = sampler.run_mcmc(pos, 200)
 
-	print "Running MCMC ..."
-	pos, prob, state = sampler.run_mcmc(pos, 500, rstate0=state)
+		sampler.reset()
 
-	flat_samples = sampler.flatchain
-	samples = sampler.chain[:, :, :].reshape((-1, ndim))
-	mc_results = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), zip(*np.percentile(samples, [16, 50, 84], axis=0)))
+		print "Running MCMC ..."
+		pos, prob, state = sampler.run_mcmc(pos, 500, rstate0=state)
 
-	return flat_samples, mc_results
+		flat_samples = sampler.flatchain
+		samples = sampler.chain[:, :, :].reshape((-1, ndim))
+		mc_results = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), zip(*np.percentile(samples, [16, 50, 84], axis=0)))
+
+		return flat_samples, mc_results
--- emission_line_fitter_mcmc.py
+++ emission_line_fitter_mcmc.py
@@ -23,23 +23,35 @@ wl_sol_file = '/Users/Briana/Documents/Grad_School/M82/ppxf/pPXF_VP/results/Fcal
 
 outname = 'F1_B_OIII_gasResid.dat'
 
-line = 'OIII_Hb'
+line = 'OIII_Hb_trip'
 z		= 0.000677
-wl_trim_range = 40
 
+thetaGuess = [z, 4, 4,4] #z, sig, inten1, (inten2)
+ndim, nwalkers = len(thetaGuess), 100
+nchains = (200, 500)
 
 #+++++++++++++++++++ end user defined variables +++++++++++++++++++++#
 
-#For triplets: need to put the strongest line first and the weaker line second. The H line is last 
-line_wls    = {'OII':[3727], 'OIII5007':[5007], 'OIII4959':[4959],'Hbonly':[4861], 'OIII_Hb':[5007,4959,4861], "NII_Ha":[6583,6549,6562], "SII":[6731,6717]}
-line_ratios = {'OIII_Hb':3, 'NII_Ha':2.5, 'OIII_doub':3}
+model_dict = {'OII':mlf.OII_gaussian,
+			'Hb': mlf.Hb_gaussian,
+			'OIII_doub': mlf.OIII_doub_gaussian,
+			'SII_doub': mlf.SII_doub_gaussian,
+			'OIII_Hb_trip': mlf.OIII_Hb_trip_gaussian,
+			'NII_Ha_trip': mlf.NII_Ha_trip_gaussian}
+
+wl_trim_dict = {'OII':(3700.0, 3760.0),
+				'Hb': (4830.0, 4890.0),
+				'OIII_doub': (4930.0, 5050.0),
+				'SII_doub': (6700.0, 6750.0),
+				'OIII_Hb_trip': (4830.0, 5050.0),
+				'NII_Ha_trip': (6520.0, 6610.0)}
 
 gasfit = np.load(gasfit_file)
 galfit = np.load(galfit_file)
 newgal = np.load(newgal_file)
 wl_sol = np.load(wl_sol_file)
 
-inds = np.where((4930.0 < wl_sol) & (wl_sol < 5050.0))
+inds = np.where((wl_trim_dict[line][0] < wl_sol) & (wl_sol < wl_trim_dict[line][1]))
 
 gasfit = gasfit[:, inds]
 galfit = galfit[:, inds]
@@ -49,21 +61,34 @@ wl_sol = wl_sol[inds]
 residuals = np.subtract(newgal, galfit)
 dat       = np.add(gasfit, residuals)
 
-thetaGuess = [z, 4, 4]
-ndim, nwalkers = len(thetaGuess), 100
-args=(wl_sol, dat[0][0], np.std(residuals[0][0])**2)
-nchains = (200, 500)
+#+++++++++++++++++++ this is where I need to add iteration over the data cube +++++++++++++++++++++#
 
-flat_samp, mc_results = elef.run_emcee(elef.lnprob, ndim, nwalkers, nchains, thetaGuess, args)
+args=(wl_sol, dat[0][0], np.std(residuals[0][0])**2)
+
+OII_MCMC = elef.MCMC_functions(model_dict[line], 20)
+
+flat_samp, mc_results = OII_MCMC.run_emcee(OII_MCMC.lnprob, ndim, nwalkers, nchains, thetaGuess, args)
 print mc_results 
 
-z_mc, sig_mc, inten_mc = mc_results 
+if len(thetaGuess) > 3:
 
-plt.plot(wl_sol, dat[0][0])
-plt.plot(wl_sol, mlf.OIII_doub_gaussian(wl_sol, theta = (z_mc[0], sig_mc[0], inten_mc[0])), color='red')
-plt.plot(wl_sol, mlf.OIII_doub_gaussian(wl_sol, theta = (z_mc[0]+z_mc[1], sig_mc[0]+sig_mc[1], inten_mc[0]+inten_mc[1])), color='red', ls=':')
-plt.plot(wl_sol, mlf.OIII_doub_gaussian(wl_sol, theta = (z_mc[0]-z_mc[2], sig_mc[0]-sig_mc[2], inten_mc[0]-inten_mc[2])), color='red', ls=':')
-plt.show()
+	z_mc, sig_mc, inten1_mc, inten2_mc = mc_results 
+
+	plt.plot(wl_sol, dat[0][0])
+	plt.plot(wl_sol, model_dict[line](wl_sol, theta = (z_mc[0], sig_mc[0], inten1_mc[0], inten2_mc[0])), color='red')
+	plt.plot(wl_sol, model_dict[line](wl_sol, theta = (z_mc[0]+z_mc[1], sig_mc[0]+sig_mc[1], inten1_mc[0]+inten1_mc[1], inten2_mc[0]+inten2_mc[1])), color='red', ls=':')
+	plt.plot(wl_sol, model_dict[line](wl_sol, theta = (z_mc[0]-z_mc[2], sig_mc[0]-sig_mc[2], inten1_mc[0]-inten1_mc[2], inten2_mc[0]-inten2_mc[2])), color='red', ls=':')
+	plt.show()
+
+else:
+
+	z_mc, sig_mc, inten_mc = mc_results 
+
+	plt.plot(wl_sol, dat[0][0])
+	plt.plot(wl_sol, model_dict[line](wl_sol, theta = (z_mc[0], sig_mc[0], inten_mc[0])), color='red')
+	plt.plot(wl_sol, model_dict[line](wl_sol, theta = (z_mc[0]+z_mc[1], sig_mc[0]+sig_mc[1], inten_mc[0]+inten_mc[1])), color='red', ls=':')
+	plt.plot(wl_sol, model_dict[line](wl_sol, theta = (z_mc[0]-z_mc[2], sig_mc[0]-sig_mc[2], inten_mc[0]-inten_mc[2])), color='red', ls=':')
+	plt.show()
 
 
 
